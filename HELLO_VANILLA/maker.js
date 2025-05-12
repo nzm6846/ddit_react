75d20ad7a6cddd596e12cf5035e8f1b1a1c95fc8
nzm <nzm6846@gmail.com>
2025-05-12 18:26:59 +0900

    바닐라 자바스크립트 과제1

--- HELLO_VANILLA/maker.js
+++ HELLO_VANILLA/maker.js
@@ -1,23 +1,23 @@
 const Vehicle = require('./vehicle.js');
 
-class Maker extends Vehicle{
-	constructor(){
+class Maker extends Vehicle {
+	constructor() {
 		super();
-		this.maker_name="mn";
+		this.maker_name = "mn";
 	}
-	
-	buyACar(mn)	{
+
+	buyACar(mn) {
 		this.maker_name = mn;
 	}
-	
+
 }
 
-if(require.main === module) {
-		const mk = new Maker("현대");
-		console.log("speed:"+mk.speed);
-		console.log("maker_name:"+mk.maker_name);
-		mk.excel();
-		mk.buyACar("포르쉐");
-		console.log("speed:"+mk.speed);
-		console.log("maker_name:"+mk.maker_name);
+if (require.main === module) {
+	const mk = new Maker("현대");
+	console.log("speed:" + mk.speed);
+	console.log("maker_name:" + mk.maker_name);
+	mk.excel();
+	mk.buyACar("포르쉐");
+	console.log("speed:" + mk.speed);
+	console.log("maker_name:" + mk.maker_name);
 }
--- HELLO_VANILLA/vehicle.js
+++ HELLO_VANILLA/vehicle.js
@@ -3,17 +3,17 @@ class Vehicle {
 		this.speed = 0;
 
 	}
-	
+
 	excel() {
 		this.speed++;
 	}
 }
 module.exports = Vehicle;
 
-if(require.main === module) {
-	
+if (require.main === module) {
+
 	const ve = new Vehicle();
-	console.log("speed: "+ve.speed);
+	console.log("speed: " + ve.speed);
 	ve.excel();
-	console.log("speed:"+ve.speed);
+	console.log("speed:" + ve.speed);
 }
