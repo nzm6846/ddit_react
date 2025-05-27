e5e1831492ed4dd206d8b9aaef2d0f5796fcaf3f
nzm <nzm6846@gmail.com>
2025-05-27 14:53:47 +0900

    ..

--- HELLO_SEVLET/src/main/java/shop/ingong/web/Myhello.java
+++ HELLO_SEVLET/src/main/java/shop/ingong/web/Myhello.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/hello")
-public class MyHello extends HttpServlet {
+public class Myhello extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
